4b7ff7689d2987345d02330aea16aa0940226858
jk-holmen <166861650+jk-holmen@users.noreply.github.com>
2026-03-13 20:06:18 +0100

    Create a base HTTP client for GET requests

---
+++ src/test/java/io/github/jk_holmen/clients/BaseClient.java
@@ -0,0 +1,34 @@
+package io.github.jk_holmen.clients;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import io.github.jk_holmen.config.ConfigurationManager;
+
+public class BaseClient {
+
+    protected final HttpClient client;
+    protected final String BASE_URL;
+
+
+    public BaseClient() {
+        client = HttpClient.newBuilder().build();
+        BASE_URL = ConfigurationManager.getInstance().getBaseUrl();
+    }
+
+    public HttpResponse<String> get(String path)
+            throws IOException, InterruptedException {
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(BASE_URL + path))
+            .build();
+
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        
+        return response;
+    }
+}
