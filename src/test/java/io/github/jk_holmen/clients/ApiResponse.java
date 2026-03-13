c4d72a2a99e9af7f720100f24f272430b5939fb7
jk-holmen <166861650+jk-holmen@users.noreply.github.com>
2026-03-13 22:56:45 +0100

    Add generic method to deserialize from JSON

--- src/test/java/io/github/jk_holmen/clients/ApiResponse.java
+++ src/test/java/io/github/jk_holmen/clients/ApiResponse.java
@@ -3,9 +3,12 @@ package io.github.jk_holmen.clients;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpResponse;
 
+import com.google.gson.Gson;
+
 public class ApiResponse {
     
     private final HttpResponse<String> httpResponse;
+    private static final Gson gson = new Gson();
 
     public ApiResponse(HttpResponse<String> httpResponse) {
         this.httpResponse = httpResponse;
@@ -21,5 +24,9 @@ public class ApiResponse {
 
     public HttpHeaders headers() {
         return httpResponse.headers();
+    }
+
+    public <T> T as(Class<T> type) {
+        return gson.fromJson(httpResponse.body(), type);
     }
 }
